c7d51c3ace1da0786df7c825d923bcae1f145ee2
Rafael <rafaelmoura7033@gmail.com>
2022-03-14 18:18:23 -0300

    Alteracoes no arquivo config.js

--- config/config.js
+++ config/config.js
@@ -1,25 +1,27 @@
 require('dotenv').config();
 
+console.log(process.env);
+
 module.exports = {
   development: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: 'blogs_api',
-    host: 'localhost',
+    host: process.env.HOSTNAME,
     dialect: 'mysql',
   },
   test: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: 'blogs_api',
-    host: 'localhost',
+    host: process.env.HOSTNAME,
     dialect: 'mysql',
   },
   production: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: 'blogs_api',
-    host: 'localhost',
+    host: process.env.HOSTNAME,
     dialect: 'mysql',
   },
 };
